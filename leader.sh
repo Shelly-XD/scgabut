41712de280bd269e3b3204e85709d2be3b2baf0f
Shelly <87390227+Shelly-XD@users.noreply.github.com>
2022-02-12 20:03:06 +0700

    Add files via upload

---
+++ leader.sh
@@ -0,0 +1,34 @@
+
+
+echo "Geser Rumah Leader Ke Mars"
+echo""
+echo "1. Geser Rumah Leader Ke Mars Bersama Waifu Dan Istri Kartun Nya"
+echo ""
+echo "Pilih No.1 Untuk Geser Rumah Leader Sampe Ke Mars Bersama Waifu Dan Istri Kartun Nya"
+read -p "[•]" pil;
+echo "Sabar Mek Mars Nya Jauh"
+echo "────────────────────────────────────────────"
+sleep 5
+echo "Leader Ga Bisa Ke Mars Mek Masih Banyak Hutang Leader Nya Mek"
+echo "─────────────────────────────────────────────"
+sleep 5
+echo "Suruh Leader Kau Bayar Hutang Ke Member Nya Dulu Mek"
+echo "────────────────────────────────────────────"
+sleep 5
+echo "Beberapa Hari Kemudian"
+echo "────────────────────────────────────────────"
+sleep 5
+echo "Hutang Leader Pun Lunas. "
+echo "────────────────────────────────────────────"
+sleep 5
+echo "Sabar Mek Rumah Leader Lagi Di Geser Ke Mars"
+echo "────────────────────────────────────────────"
+sleep 3
+echo "Leader Bamyak Dosa Mek Jadi Geser Rumah Nya Susah Tunggu 100 Tahun Lagi Mek"
+echo "────────────────────────────────────────────"
+sleep 5
+echo "Setelah 100 Tahun Berlalu Rumah Leader Pun Ke Geser Ke Mars"
+echo "────────────────────────────────────────────"
+sleep 10
+echo "Selamat Rumah Leader Anda Tergeser Ke Mars"
+exit
